df96a2b564c693451b67ce8a00c545341d5e1e1a
shardulgawande27 <shardulgawande27@gmail.com>
2021-10-10 16:44:26 +0530

    commit7

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $name= $_POST['name'];
+    $emailfrom= $_POST['email'];
+    $phone= $_POST['phone'];
+    $message= $_POST['message'];
+}
+
+$mailTo = "sahil.darge.2219@gmail.com";
+$headers = "From: ".$mailfrom;
+$txt = "You have recievded an email from".$name.".\n\n".$message;
+
+mail($mailTo, $subject, $txt, $headers);
+header("Location: contact1.html?mailsend");
+?>
